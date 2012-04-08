18549cff7cce5d0d5ac87315b696dacec35512a1
stonestylesun123 <stonestylesun123@gmail.com>
2012-04-09 00:57:41 +0800

    step11 完成第三个功能。 2012-04-09

--- submanagerUI.py
+++ submanagerUI.py
@@ -288,8 +288,153 @@ class submanagerUI(wx.Panel):
                     exec(command)
 
     def book_manager_UI(self):
-        pass
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        sizer = wx.GridBagSizer(1,7)
+        u3_st0 = wx.StaticText(self, -1, label="图书管理")
+        u3_st0.SetForegroundColour('#3014D4')
+        u3_st1 = wx.StaticText(self, -1, label="请输入书号")
+        self.u3_tc1 = wx.TextCtrl(self, -1, size=(105, 26))
+        u3_bt1 = wx.Button(self, 401, label='查询')
+        u3_new_bt1 = wx.Button(self, 402, label='删除图书')
+        u3_new_bt2 = wx.Button(self, 403, label='编辑图书信息')
+        u3_new_bt3 = wx.Button(self, 404, label='图书入库')
+        sizer.Add(u3_st0, pos=(0,0), flag=wx.ALL, border=15)
+        sizer.Add(u3_st1, pos=(0,1), flag=wx.ALL, border=15)
+        sizer.Add(self.u3_tc1, pos=(0,2), flag=wx.ALL, border=12)
+        sizer.Add(u3_bt1, pos=(0,3), flag=wx.ALL, border=9)
+        sizer.Add(u3_new_bt1, pos=(0,4), flag=wx.ALL, border=9)
+        sizer.Add(u3_new_bt2, pos=(0,5), flag=wx.ALL, border=9)
+        sizer.Add(u3_new_bt3, pos=(0,6), flag=wx.ALL, border=9)
+        vbox.Add(sizer)
+        line = wx.StaticLine(self)
+        vbox.Add(line, proportion=0, flag=wx.ALL|wx.EXPAND, border=10)
+        sizer2 = wx.GridBagSizer(5,4)
+        u3_st2 = wx.StaticText(self, -1, label="图书信息")
+        u3_st2.SetForegroundColour('#3014D4')
+        u3_st3 = wx.StaticText(self, -1, label="id")
+        self.u3_tc3 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc3.Disable()
+        u3_st4 = wx.StaticText(self, -1, label="name")
+        self.u3_tc4 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc4.Disable()
+        u3_st5 = wx.StaticText(self, -1, label="author")
+        self.u3_tc5 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc5.Disable()
+        u3_st6 = wx.StaticText(self, -1, label="publisher")
+        self.u3_tc6 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc6.Disable()
+        u3_st7 = wx.StaticText(self, -1, label="type")
+        self.u3_tc7 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc7.Disable()
+        u3_st8 = wx.StaticText(self, -1, label="amount")
+        self.u3_tc8 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc8.Disable()
+        u3_st9 = wx.StaticText(self, -1, label="lended_amount")
+        self.u3_tc9 = wx.TextCtrl(self, -1, size=(180, 26))
+        self.u3_tc9.Disable()
+        u3_st10= wx.StaticText(self, -1, label="remarks")
+        self.u3_tc10= wx.TextCtrl(self, -1, size=(240, 26))
+        self.u3_tc10.Disable()
+        sizer2.Add(u3_st2, pos=(0,0), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st3, pos=(1,0), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc3, pos=(1,1), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st4, pos=(1,2), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc4, pos=(1,3), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st5, pos=(2,0), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc5, pos=(2,1), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st6, pos=(2,2), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc6, pos=(2,3), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st7, pos=(3,0), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc7, pos=(3,1), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st8, pos=(3,2), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc8, pos=(3,3), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st9, pos=(4,0), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc9, pos=(4,1), flag=wx.ALL, border=5)
+        sizer2.Add(u3_st10,pos=(4,2), flag=wx.ALL, border=5)
+        sizer2.Add(self.u3_tc10,pos=(4,3), flag=wx.ALL, border=5)
+        vbox.Add(sizer2, proportion=0, flag=wx.LEFT|wx.EXPAND, border=15)
+        self.u3_lookup = False
+        self.u3_edit = False
+        self.u3_handon = False
+        self.Bind(wx.EVT_BUTTON, self.bookmanage, u3_bt1)
+        self.Bind(wx.EVT_BUTTON, self.bookmanage, u3_new_bt1)
+        self.Bind(wx.EVT_BUTTON, self.bookmanage, u3_new_bt2)
+	self.Bind(wx.EVT_BUTTON, self.bookmanage, u3_new_bt3)
 
+        return vbox
+
+    def bookmanage(self, e):
+	if e.GetId() == 401:
+	    string = self.u3_tc1.GetValue()
+	    if string == "":
+                wx.MessageBox('Invalid input!', 'Error', wx.OK | wx.ICON_ERROR)
+            else:
+		for i in range(8):
+		    command = "self.u3_tc%d.Disable()" % (i + 3)
+		    exec(command)
+                result = self.lib_manager.lookup_Book_by_ID(string)
+                if result == None or result == False:
+                    wx.MessageBox('Does not exist', 'Error', wx.OK | wx.ICON_ERROR)
+                else:
+                    print result
+                    for i in range(len(result)):
+                        command = """self.u3_tc%d.SetValue("%s")""" % ((i + 3), result[i])
+                        exec(command)
+		    self.u3_lookup = True
+	if e.GetId() == 402:
+	    if not self.u3_lookup:
+		wx.MessageBox('Select a book first!', 'Error', wx.OK | wx.ICON_ERROR)
+	    else:
+                dial = wx.MessageDialog(None, 'Are you sure to delete?', 'Question', wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+                ret = dial.ShowModal()
+		if ret == wx.ID_YES:
+		    string = self.u3_tc3.GetValue()
+		    self.lib_manager.del_Book(string)
+		    self.u3_lookup = False
+		    for i in range(8):
+                        command = """self.u3_tc%d.SetValue("")""" % (i + 3)
+                        exec(command)
+		else:
+		    return
+	if e.GetId() == 403:
+	    for i in range(8):
+		command = """self.u3_tc%d.SetValue("")""" % (i + 3)
+		exec(command)
+		command = "self.u3_tc%d.Enable()" % (i + 3)
+		exec(command)
+	    self.u3_lookup = False
+	    self.u3_edit = True
+	if e.GetId() == 404:
+	    if not self.u3_edit:
+		wx.MessageBox('Editing first!', 'Error', wx.OK | wx.ICON_ERROR)
+	    else:
+                dial = wx.MessageDialog(None, 'Are you sure to add books?', 'Question', wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+                ret = dial.ShowModal()
+		if ret == wx.ID_YES:
+                    temp = []
+                    for i in range(8):
+                    	command = "tmp = self.u3_tc%d.GetValue()" % (i + 3)
+                    	exec(command)
+                    	temp.append(tmp)
+                    if temp[0] == "":
+                        wx.MessageBox('Invalid input', 'Error', wx.OK | wx.ICON_ERROR)
+			return
+		    result = self.lib_manager.lookup_Book_by_ID(temp[0])
+                    if not result == False:
+                        wx.MessageBox('Invalid input or duplicate ID', 'Error', wx.OK | wx.ICON_ERROR)
+			return
+		    print temp
+		    self.lib_manager.add_Book(temp)
+		    self.u3_edit = False
+		    for i in range(8):
+                        command = """self.u3_tc%d.SetValue("")""" % (i + 3)
+                        exec(command)
+			command = "self.u3_tc%d.Disable()" % (i + 3)
+			exec(command)     
+		else:
+		    return 
+		
     def reader_manager_UI(self):
         pass
 
